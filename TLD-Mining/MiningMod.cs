76a4bba5cc19b0324783f971fdfb0006d03e45f4
Samimies <3jti3jt2jngns.3jti3jt2jngns@3jti3jt2jngns25959595959201010102002.co>
2023-06-03 00:24:52 +0300

    `Initial Commit`

---
+++ TLD-Mining/MiningMod.cs
@@ -0,0 +1,12 @@
+﻿using MelonLoader;
+
+namespace TLD_Mining
+{
+    public class MiningMod : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            MelonDebug.Msg("[MiningMod] OnInitializeMelon");
+        }
+    }   
+}
